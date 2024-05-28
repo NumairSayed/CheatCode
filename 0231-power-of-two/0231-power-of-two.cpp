eb2e91452a7c770eb3293137bb7838d6fe5c10fd
SlimShadybrvh <107948730+SlimShadybrvh@users.noreply.github.com>
2024-05-28 13:05:27 +0530

    Delete 0231-power-of-two directory

--- 0231-power-of-two/0231-power-of-two.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    bool isPowerOfTwo(int n) {
-        if(n==0 || (((n%2))&&n>1)) return false;
-        else if (n==1) return true;
-        else if(!(n%2)) return isPowerOfTwo(n/2);
-        return false;
-    }
-};
