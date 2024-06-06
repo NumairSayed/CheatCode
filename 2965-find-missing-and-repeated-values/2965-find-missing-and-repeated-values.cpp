86509f01865972d38f65dc6fb3e1779b54347855
NUMAIR SAYED <107948730+SlimShadybrvh@users.noreply.github.com>
2024-06-06 21:10:17 +0530

    Time: 18 ms (77.50%), Space: 25 MB (91.24%) - LeetHub

---
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        
+        int sum =0;
+    int n = pow(grid.size(),2);
+    for(auto &it: grid)
+    {
+        for(auto &bit: it)
+        {
+            sum+=bit;
+        }
+    }
+        long long actualSum =n*(n+1)/2; 
+    int diff = sum - actualSum;
+    
+    long long sumOfSquares=0;
+    for(auto &it: grid)
+    {
+        for(auto &bit: it)
+        {
+            sumOfSquares+=pow(bit,2);
+        }
+    }
+    
+    long long squarediff = sumOfSquares - actualSum*(2*n+1)/3;
+    int aplusb = squarediff/diff;
+    int a = (diff+aplusb)/2;
+    int b = aplusb - a;
+    vector<int> output;
+    output.push_back(a);
+    output.push_back(b);
+    return output;
+    }
+};
