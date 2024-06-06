896c549a676b918195ebc0fd7e794e4c76d84343
NUMAIR SAYED <107948730+SlimShadybrvh@users.noreply.github.com>
2024-06-06 23:39:18 +0530

    Time: 0 ms (100.00%), Space: 11 MB (71.77%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        if(!m) {nums1=nums2;return;}
+        if(!n) return;
+       // if(!m && n) {swap(nums1[0],nums2[0]); return;}
+        int i=m-1;int j=n-1; int k =m+n-1;
+        while(i>=0 && j>=0)
+        {
+            if(nums1[i]>nums2[j])
+            {
+                nums1[k]=nums1[i]; k--;i--;
+            }
+            else
+            {
+                nums1[k]=nums2[j]; j--;k--;
+            }
+        }
+        
+        if(i>=0 && j<0) return;
+        if(j>=0 && i<0) 
+        {
+            while(j>=0)
+            {
+                nums1[k]=nums2[j];j--;k--;
+            }
+        }
+        if(j>=0 && i>=0)
+        {
+            while(i>=0 && j>=0)
+        {
+            if(nums1[i]>nums2[j])
+            {
+                nums1[k]=nums1[i]; k--;i--;
+            }
+            else
+            {
+                nums1[k]=nums2[j]; j--;k--;
+            }
+        }
+        }
+        
+        
+        
+        
+        return;
+    }
+};
