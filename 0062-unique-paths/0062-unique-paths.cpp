978237c4cb32f7df3122beddc839481c6e8532af
NUMAIR SAYED <107948730+SlimShadybrvh@users.noreply.github.com>
2024-06-12 20:04:07 +0530

    Time: 3 ms (32.92%), Space: 7.2 MB (86.38%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,16 @@
+constexpr inline size_t binom(size_t n, size_t k) noexcept
+{
+    return
+      (        k> n  )? 0 :          // out of range
+      (k==0 || k==n  )? 1 :          // edge
+      (k==1 || k==n-1)? n :          // first
+      binom(n - 1, k - 1) * n / k;   // recursive
+}
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        
+      return binom(n+m-2,min(m,n)-1);
+    }
+};
