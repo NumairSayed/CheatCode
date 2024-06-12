0fc419af25c09f18efb02c3ce5ed1154500a6598
NUMAIR SAYED <107948730+SlimShadybrvh@users.noreply.github.com>
2024-06-12 19:07:11 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (86.38%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,22 +1,16 @@
-unsigned long long binomialCoefficient(int n, int k) {
-    if (k > n) return 0;
-    if (k == 0 || k == n) return 1;
-
-    // C(n, k) is the same as C(n, n-k)
-    if (k > n - k) k = n - k;
-
-    unsigned long long res = 1;
-    for (int i = 0; i < k; ++i) {
-        res *= (n - i);
-        res /= (i + 1);
-    }
-    return res;
+constexpr inline size_t binom(size_t n, size_t k) noexcept
+{
+    return
+      (        k> n  )? 0 :          // out of range
+      (k==0 || k==n  )? 1 :          // edge
+      (k==1 || k==n-1)? n :          // first
+      binom(n - 1, k - 1) * n / k;   // recursive
 }
 
 class Solution {
 public:
     int uniquePaths(int m, int n) {
         
-      return binomialCoefficient(n+m-2,m-1);
+      return binom(n+m-2,m-1);
     }
 };
